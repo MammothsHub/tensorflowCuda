953b33b2ae715270153b9a4e9caaf5ea79af35b2
MammothsHub <MammothsHub@github.com>
2024-01-03 13:12:06 +0000

    changed the exportSpackCuda and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -16,8 +16,6 @@ RUN git clone https://github.com/spack/spack.git && \
     source /spack/share/spack/setup-env.sh && \
     spack install cuda@12.2.0 
 
-#RUN echo "source /spack/share/spack/setup-env.sh && spack load cuda && export CUDA_DIR=$CUDA_HOME && export CUDA_PATH=$CUDA_HOME && export CUDA_ROOT=$CUDA_HOME && export CUDA_BIN=$CUDA_HOME/bin && export CUDA_LIB=$CUDA_HOME/lib64 && export CUDA_VER=12.2.0 && export CUDA_INSTALL_PATH=$CUDA_HOME && export CUDA_BIN_PATH=$CUDA_HOME && export PATH=$PATH:$CUDA_BIN && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_LIB && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/extras/CUPTI/lib64 && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/include && export CPLUS_INCLUDE_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/include && export MANPATH=$CUDA_HOME/man " >> exportSpackCuda.sh
-
 COPY exportSpackCuda.sh /
 
 RUN chmod ugo+wrx /exportSpackCuda.sh && \
@@ -39,5 +37,5 @@ RUN /exportSpackCuda.sh && \
     fix-permissions "/home/${NB_USER}" 
 
 #Export spack's cuda 
-#CMD /exportSpackCuda.sh     
+CMD source /exportSpackCuda.sh     
     
--- exportSpackCuda.sh
+++ exportSpackCuda.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-source spack/share/spack/setup-env.sh && \
-    spack load cuda && \
-    export CUDA_DIR=$CUDA_HOME && \
-    export CUDA_PATH=$CUDA_HOME && \
-    export CUDA_ROOT=$CUDA_HOME && \
-    export CUDA_BIN=$CUDA_HOME/bin && \
-    export CUDA_LIB=$CUDA_HOME/lib64 && \ 
-    export CUDA_VER=12.2.0 && \
-    export CUDA_INSTALL_PATH=$CUDA_HOME && \
-    export CUDA_BIN_PATH=$CUDA_HOME && \
-    export PATH=$PATH:$CUDA_BIN && \ 
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_LIB && \
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/extras/CUPTI/lib64 && \
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/include && \
-    export CPLUS_INCLUDE_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/include && \
-    export MANPATH=$CUDA_HOME/man 
+source /spack/share/spack/setup-env.sh 
+    spack load cuda 
+    export CUDA_DIR=$CUDA_HOME  
+    export CUDA_PATH=$CUDA_HOME 
+    export CUDA_ROOT=$CUDA_HOME 
+    export CUDA_BIN=$CUDA_HOME/bin 
+    export CUDA_LIB=$CUDA_HOME/lib64   
+    export CUDA_VER=12.2.0 
+    export CUDA_INSTALL_PATH=$CUDA_HOME 
+    export CUDA_BIN_PATH=$CUDA_HOME 
+    export PATH=$PATH:$CUDA_BIN  
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_LIB 
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/extras/CUPTI/lib64 
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/include 
+    export CPLUS_INCLUDE_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/include 
+    export MANPATH=$CUDA_HOME/man
+
